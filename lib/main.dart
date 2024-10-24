532762878bf6c2ebce7255a40d472206d80cedc8
James <pcjsu@naver.com>
2024-10-24 23:18:07 +0900

    scroll add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_app/layout.dart';
+import 'package:login_app/lottery_app/lotto.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Layout(),
+      theme: ThemeData(colorSchemeSeed: Colors.blue),
+      home: const Lotto(),
     );
   }
 }
